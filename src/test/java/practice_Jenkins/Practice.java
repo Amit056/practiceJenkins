6363da36317bfdbb4ac3fe79474db46cfca018ff
akray <akray0413@gmail.com>
2022-12-20 15:16:15 +0530

    commitfOrJenkins

---
+++ src/test/java/practice_Jenkins/Practice.java
@@ -0,0 +1,12 @@
+package practice_Jenkins;
+
+import org.testng.annotations.Test;
+
+public class Practice {
+	@Test
+	public void test()
+	{
+		System.out.println("practice was executed");
+	}
+
+}
