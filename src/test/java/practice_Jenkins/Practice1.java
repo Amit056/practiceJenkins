55d9200f77bcc51cc447128fb57a0d62321bc8ee
akray <akray0413@gmail.com>
2022-12-20 15:54:05 +0530

    practice2 was committed

---
+++ src/test/java/practice_Jenkins/Practice1.java
@@ -0,0 +1,12 @@
+package practice_Jenkins;
+
+import org.testng.annotations.Test;
+
+public class Practice1 {
+	@Test
+	public void test()
+	{
+		System.out.println("practice1 was executed");
+	}
+
+}
